101c108b11ddb12336ad5eb3cb50c40942a991d3
AmritoRock <amritokarmokar5i@gmail.com>
2024-09-19 22:08:19 +0600

    r5x: Bringup DroidX-UI

--- droidx_r5x.mk
+++ droidx_r5x.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -21,7 +21,11 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := aosp_r5x
+PRODUCT_NAME := droidx_r5x
 PRODUCT_MODEL := realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# DroidX-UI stuff
+DROIDX_BUILD_TYPE := OFFICIAL
+DROIDX_GAPPS := true
